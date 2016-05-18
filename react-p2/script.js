e80ccac6af7ada257f9fc6067ad49a00e8bf32cf
Jean Farias Roldão <jeanroldao@gmail.com>
2016-05-18 16:51:12 -0300

    Card component
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

---
+++ react-p2/script.js
@@ -0,0 +1,38 @@
+
+var Card = React.createClass({
+  
+  getInitialState: function() {
+    return {};
+  },
+  
+  componentDidMount: function() {
+    var component = this;
+    $.get('https://api.github.com/users/' + this.props.login, function (data) {
+      component.setState(data);
+    });
+  },
+  
+  render: function() {
+    return (
+      <div>
+        <img src={this.state.avatar_url} width="80" />
+        <h3>{this.state.name}</h3>
+        <hr />
+      </div>
+    );
+  }
+});
+
+var Main = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        <Card login="jeanroldao" />
+        <Card login="petehunt" />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Main />, document.getElementById('root'));
